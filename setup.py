357026b3fab9e6a6a80a20f7363203d2f4744574
olcaytaner <olcaytaner@gmail.com>
2024-10-31 14:30:41 +0300

    Initial commit

---
+++ setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup
+
+from pathlib import Path
+this_directory = Path(__file__).parent
+long_description = (this_directory / "README.md").read_text(encoding="utf-8")
+
+setup(
+    name='NlpToolkit-SequenceProcessing',
+    version='1.0.0',
+    packages=['SequenceProcessing', 'SequenceProcessing.Classification', 'SequenceProcessing.Initializer',
+              'SequenceProcessing.Labeling', 'SequenceProcessing.Sequence'],
+    url='https://github.com/StarlangSoftware/SequenceProcessing-Py',
+    license='',
+    author='olcaytaner',
+    author_email='olcay.yildiz@ozyegin.edu.tr',
+    description='Sequence Processing library',
+    install_requires=['NlpToolkit-Math', 'NlpToolkit-WordToVec', 'NlpToolkit-Corpus', 'NlpToolkit-Classification'],
+    long_description=long_description,
+    long_description_content_type='text/markdown'
+)
